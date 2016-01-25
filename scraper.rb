0629935a9a3f5752d81e4ce7e2a53c1131f9e3d8
Tony Bowden <tony@tmtm.com>
2016-01-25 07:27:40 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,13 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'wikidata/fetcher'
+
+en_names = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/List_of_current_members_of_the_National_Assembly_of_Serbia',
+  xpath: '//table[.//th[contains(.,"Party")]]//tr//td[1]//a[not(@class="new")][1]/@title',
+) 
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names }, output: true)
+warn EveryPolitician::Wikidata.notify_rebuilder
+
